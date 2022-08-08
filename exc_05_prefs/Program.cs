82b284ec1d75e54cca1d7d79e3f9bbac1c68aff2
Andrew <vishnyaishere@gmail.com>
2022-08-08 14:28:37 +0300

    added exc_05

---
+++ exc_05_prefs/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "andrew")
+{
+    Console.WriteLine("hi helo frend");
+}
+else
+{
+    Console.WriteLine("Hello, " + username);
+}
